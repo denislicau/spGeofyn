5dfce4a2a5d55ae0a03c66a5b532db6421fb7b34
Omar <omar0954@edu.eal.dk>
2016-05-17 13:08:35 +0200

    Rechanged title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="jumbotron">
 		<div id="LogoHolder">
 			<img id="LogoImg" src="logo.png">
-			 <h2>Geofyn's smartcity viewer github</h2>      
+			 <h2>Geofyn's smartcity viewer gouthhub</h2>      
 		</div>
 	</div>
 </div>
