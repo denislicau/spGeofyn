f9205513137b5f1fff9a34f72e16f6a6c692d766
Denis Licau <denisl.pericol@gmail.com>
2016-05-17 12:09:32 +0200

    Add files via upload

--- maincontent.php
+++ maincontent.php
@@ -1,7 +1,7 @@
 <div id="Content">
 	<nav class="sideNavLeft col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2">
 	    <ul>
-	      <li><a href="#">1</a></li>
+	      <li style="border-top: none;"><a href="#">1</a></li>
 	      <li><a href="#">2</a></li>
 	      <li><a href="#">3</a></li>
 	      <li><a href="#">4</a></li>
@@ -16,19 +16,13 @@
 				<li>We need a statistics page</li>
 				<li>We need to create a place for registering V.I.P(laces) from Fyn</li>
 			</ul> -->
-<!-- 			<iframe width="420" height="600"
-src="http://www.youtube.com/embed/XGSy3_Czz8k?autoplay=1">
-</iframe> -->
+		<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d290655.8589948922!2d10.4274656!3d55.3100076!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sdk!4v1463469384519" width="100%" height="470" frameborder="0" style="border:0" allowfullscreen></iframe>
 	</div>
 	<nav class="sideNavRight col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2">
-	    <ul>
-	      <li><a href="#">1</a></li>
-	      <li><a href="#">2</a></li>
-	      <li><a href="#">3</a></li>
-	      <li><a href="#">4</a></li>
-	      <li><a href="#">5</a></li>
-	      <li><a href="#">6</a></li>
-	    </ul>
+	    <h4>Location:<br>Park</h4>
+	    <p>Address: odensevej 22<br>
+	    Visited(last day): 800 times<br>
+	    Average visitors(month): 450/day</p>
 	</nav>
 	<div class="clearfix"></div>
 </div>
