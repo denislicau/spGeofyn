f04f6364ff1caab24b4ab20f123e834c44242d10
IamHimOmar <omar0954@edu.eal.dk>
2016-05-17 14:51:25 +0200

    Title changed
    yes

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="jumbotron">
 		<div id="LogoHolder">
 			<img id="LogoImg" src="logo.png">
-			 <h2>Geofyn's smartcity viewer test change</h2>      
+			 <h2>Geofyn's smartcity viewer WORK NOW WORK</h2>      
 		</div>
 	</div>
 </div>
