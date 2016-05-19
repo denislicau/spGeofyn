3095a64b2322585bf06a305244b75bcd02bb989d
IamHimOmar <omar0954@edu.eal.dk>
2016-05-19 13:18:01 +0200

    Login container update

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 // include the template parser class
 require('parser.php');
 // instantiate a template Parser object
-$parser=new htmlParser('template_login.html');
+$parser=new htmlParser('loginPage.html');
 // define the key-value array
 $keyValueArray=array('title'=>'GEOFYN','header'=>'header.php','maincontent'=>'maincontent.php',
-'footer'=>'footer.php');
+'footer'=>'footer.php', 'login'=>'login.php');
 
 // replace placeholders with values
 $parser->parseHtml($keyValueArray);
---
+++ login.php
@@ -0,0 +1,42 @@
+<div class="form">
+    <ul class="tab-group">
+        <li class="option active"><a href="#signup">Sign Up</a></li>
+        <li class="option"><a href="#login">Log In</a></li>
+    </ul>
+    <div class="tab-content">
+      <div id="signup">
+        <h1>Here you can sign up </h1>
+        <form action="/" method="post">
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                First Name <span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off">
+            </div>
+            <div class="field-wrap">
+              <label>
+                Last Name <span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off">
+            </div>
+          </div>
+          <div class="field-wrap">
+            <label>
+              Email address <span class="req">*</span>
+            </label>
+            <input type="text" required autocomplete="off">
+          </div>
+          <div class="field-wrap">
+            <label>
+              Password <span class="req">*</span>
+            </label>
+            <input type="text" required autocomplete="off">
+          </div>
+        </form>
+      </div>
+      <div id="login">
+        <h1>Here you can Log in</h1>
+      </div>
+    </div>
+</div>
