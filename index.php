91a39530d3e25ed289417233f72cb0674d5578e1
IamHimOmar <omar0954@edu.eal.dk>
2016-05-23 17:37:04 +0200

    change template on logging

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 require('includes.php');
 require('parser.php');
 // instantiate a template Parser object
-$parser=new htmlParser('loginPage.html');
+session_start();
+if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1)) {
+  $parser=new htmlParser('mainPage.html');	
+}
+else {
+  $parser=new htmlParser('loginPage.html');
+}
 // define the key-value array
 $keyValueArray=array('title'=>'GEOFYN','header'=>'templates/header.php','maincontent'=>'templates/maincontent.php',
 'footer'=>'templates/footer.php', 'login'=>'templates/login.php');
-
-session_start();
-if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1)) {
-	$parser=new htmlParser('mainPage.html');
-}
 // replace placeholders with values
 $parser->parseHtml($keyValueArray);
 // display the page
